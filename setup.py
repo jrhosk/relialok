f9a9ba13acd28bb695998a38a6adf60e94853e32
Josh <jhoskins@jlab.org>
2020-03-23 22:11:42 -0400

    Added file to prepare for future API and requirements to run GUI.
    * requirements.txt
    * setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="relialok-pkg-jrhosk", # Replace with your own username
+    version="0.1.1",
+    author="Joshua Hoskins",
+    author_email="jhoskins@jlab.org",
+    description="A front-end GUI for the Relialok interlocking system in use at Fermi National Accelerator Laboratory.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jrhosk/relialok",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
